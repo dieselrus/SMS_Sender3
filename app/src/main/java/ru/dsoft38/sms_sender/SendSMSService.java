afd0dc10014037a7edd290a150909138c78bfc9c
Denis Gamza <diesel_ru@mail.ru>
2015-06-11 16:40:51 +0800

    send sms timeout

--- app/src/main/java/ru/dsoft38/sms_sender/SendSMSService.java
+++ app/src/main/java/ru/dsoft38/sms_sender/SendSMSService.java
@@ -100,6 +100,12 @@ public class SendSMSService  extends Service {
         //if (currentSMSNumberIndex >= maxSMSIndex - 1)
         //        stopSelf();
 
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         if ( currentSMSNumberIndex >= maxSMSIndex ) {
             sendDataToApp("SMSSenderServiceStatus", "endtask", "end");
             stopSelf();
